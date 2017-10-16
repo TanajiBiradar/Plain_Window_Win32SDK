b9c2e36384161e9abf3dbd8ab90ddba6cb764382
TanajiBiradar <tanaji.biradar57@gmail.com>
2017-10-16 13:59:27 +0530

    Add files via upload

---
+++ PlainWindowWin32SDK.cpp
@@ -0,0 +1,63 @@
+#include<Windows.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	HWND hwnd;
+	WNDCLASSEX wndClass;
+	MSG msg;
+	TCHAR AppName[] = TEXT("Plain Window");
+
+	wndClass.cbClsExtra = NULL;
+	wndClass.cbSize = sizeof(WNDCLASSEX);
+	wndClass.cbWndExtra = NULL;
+	wndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndClass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	wndClass.hInstance = hInstance;
+	wndClass.lpfnWndProc = WndProc;
+	wndClass.lpszClassName = AppName;
+	wndClass.lpszMenuName = NULL;
+	wndClass.style = CS_HREDRAW | CS_VREDRAW;
+
+	RegisterClassEx(&wndClass);
+
+	hwnd = CreateWindow(AppName, TEXT("Plain Window"), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);
+
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return((int)msg.wParam);
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (iMsg)
+	{
+	case WM_CREATE:
+		break;
+
+	case WM_CLOSE:
+		DestroyWindow(hwnd);
+
+	case WM_KEYDOWN:
+		if (wParam == 70)
+			ShowWindow(hwnd, SW_MAXIMIZE);
+		else if (wParam == 77)
+			ShowWindow(hwnd, SW_MINIMIZE);
+		break;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}
+	return(DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
